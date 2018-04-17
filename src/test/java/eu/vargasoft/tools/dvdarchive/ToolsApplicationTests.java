df4896d06fa0132e3866e18f480edd80e9669f56
buxi <buxibuxi@gmail.com>
2018-04-17 11:24:09 +0200

    integration test introduced

--- src/test/java/eu/vargasoft/tools/dvdarchive/ToolsApplicationTests.java
+++ src/test/java/eu/vargasoft/tools/dvdarchive/ToolsApplicationTests.java
@@ -3,12 +3,12 @@ package eu.vargasoft.tools.dvdarchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("dev")
+@TestPropertySource(locations = "classpath:application-integrationtest.properties")
 public class ToolsApplicationTests {
 
 	@Test
