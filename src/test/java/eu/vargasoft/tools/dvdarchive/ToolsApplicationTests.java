53ae24147f4431e8dc6ce1cc76401b24878e4b28
buxi <buxibuxi@gmail.com>
2018-04-17 12:12:35 +0200

    removing integration test

--- src/test/java/eu/vargasoft/tools/dvdarchive/ToolsApplicationTests.java
+++ src/test/java/eu/vargasoft/tools/dvdarchive/ToolsApplicationTests.java
@@ -3,12 +3,12 @@ package eu.vargasoft.tools.dvdarchive;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@TestPropertySource(locations = "classpath:application-integrationtest.properties")
+// @TestPropertySource(locations =
+// "classpath:application-integrationtest.properties")
 public class ToolsApplicationTests {
 
 	@Test
