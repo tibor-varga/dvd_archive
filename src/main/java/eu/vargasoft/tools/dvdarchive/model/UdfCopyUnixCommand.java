e18383458b01cb4d1dc680bdfb1a26f4f35438b3
buxi <buxibuxi@gmail.com>
2018-04-18 10:15:55 +0200

    fixing naming problem in dd

--- src/main/java/eu/vargasoft/tools/dvdarchive/model/UdfCopyUnixCommand.java
+++ src/main/java/eu/vargasoft/tools/dvdarchive/model/UdfCopyUnixCommand.java
@@ -3,6 +3,7 @@
  */
 package eu.vargasoft.tools.dvdarchive.model;
 
+import eu.vargasoft.tools.dvdarchive.utils.UnixCommandExecutorHelper;
 import eu.vargasoft.tools.dvdarchive.utils.UnixCommands;
 
 /**
@@ -18,6 +19,7 @@ public class UdfCopyUnixCommand implements DiscCopyUnixCommand {
 	 */
 	@Override
 	public String getUnixCommand(final Disc disc, final String destination) {
-		return String.format(UnixCommands.DD, disc.getMountPoint(), destination);
+		return String.format(UnixCommands.DD, disc.getMountPoint(), destination,
+				UnixCommandExecutorHelper.generateDirectoryNameFromDiscLabel(disc.getLabel()));
 	}
 }
