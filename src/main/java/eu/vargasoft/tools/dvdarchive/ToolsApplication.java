f96f2efd333800c146825af2f4394ef830cf4cee
buxi <buxibuxi@gmail.com>
2018-04-19 11:55:22 +0200

    fixing sonar issues

--- src/main/java/eu/vargasoft/tools/dvdarchive/ToolsApplication.java
+++ src/main/java/eu/vargasoft/tools/dvdarchive/ToolsApplication.java
@@ -21,26 +21,6 @@ public class ToolsApplication implements CommandLineRunner {
 		SpringApplication.run(ToolsApplication.class, args);
 
 	}
-	// TODO Taskboard ---------------------------------------------------------
-	// TODO Taskboard DONE Profile introduce for UnixCommandExecutor versions
-	// TODO Taskboard DONE get mount points
-	// TODO Taskboard DONE get disc info
-	// TODO Taskboard DONE command for open tray
-	// TODO Taskboard DONE command for close tray
-	// TODO Taskboard DONE copy commands for different type of dvds
-	// TODO Taskboard DONE implementation of CopyManager
-	// TODO Taskboard DONE Fix CopyManagerTest tests
-	// TODO Taskboard DONE Jenkins onto rechner
-	// TODO Taskboard DONE parameters from properties
-	// TODO Taskboard DONE define target directories for cp and dd unix commands
-	// TODO Taskboard DONE parallelizatin of copy tasks
-	// TODO Taskboard DONE activate Sonarqube
-
-	// TODO Taskboard Todo refactor mountPoint->drive, directory->mountPoint
-	// TODO Taskboard Todo refactor Check System (unix commands, target dir, etc.)
-	// TODO Taskboard Todo IT tests with real dvds
-	// TODO Taskboard Todo implementation of Audo CD Support
-	// TODO Taskboard Todo Profiles in Spring Boot Start missing
 
 	@Override
 	public void run(String... args) throws Exception {
